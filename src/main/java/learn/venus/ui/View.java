26ab8e371b95ac64adb224cf2e40993760de6306
AErdenberger <erdenberger.alexander@gmail.com>
2023-11-03 13:25:12 -0700

    printHeader and displayMenu functioning properly

--- src/main/java/learn/venus/ui/View.java
+++ src/main/java/learn/venus/ui/View.java
@@ -6,7 +6,22 @@ public class View {
 
     private final Scanner console = new Scanner(System.in);
     public MenuOption displayMenuAndSelect(){
+        MenuOption[] values = MenuOption.values();
+        printHeader("Main Menu");
+        for(int i = 0; i < values.length; i++) {
+            System.out.printf("%s: %s%n", i, values[i].getTitle());
+        }
         return MenuOption.EXIT;
+    }
+
+    public void printHeader(String message){
+        System.out.println();
+        System.out.println(message);
+        System.out.println("=".repeat(message.length()));
+    }
+
+    private String readString(String prompt){
+
     }
 
 }
