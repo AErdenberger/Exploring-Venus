7dc697b2ffc8186b5df14cee3f3078c3d82204d0
AErdenberger <erdenberger.alexander@gmail.com>
2023-11-03 13:08:37 -0700

    run switch functional

--- src/main/java/learn/venus/ui/Controller.java
+++ src/main/java/learn/venus/ui/Controller.java
@@ -13,6 +13,22 @@ public class Controller {
     }
 
     public void run(){
-        System.out.println("Works!");
+        MenuOption option;
+        do {
+            option = view.displayMenuAndSelect();
+            switch(option){
+                case EXIT:
+                    System.out.println(option.getTitle());
+                    break;
+                case DISPLAY_ORBITERS:
+                    break;
+                case CREATE_ORBITER:
+                    break;
+                case UPDATE_ORBITER:
+                    break;
+                case DELETE_ORBITER:
+                    break;
+            }
+        } while(option != MenuOption.EXIT);
     }
 }
--- src/main/java/learn/venus/ui/View.java
+++ src/main/java/learn/venus/ui/View.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class View {
 
     private final Scanner console = new Scanner(System.in);
-
+    public MenuOption displayMenuAndSelect(){
+        return MenuOption.EXIT;
+    }
 
 }
